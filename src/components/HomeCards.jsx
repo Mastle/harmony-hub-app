4ce864dcbb4e6323e601c7ce269684674d497d34
Mastle <amirali.tsh13@gmail.com>
2025-02-19 16:45:22 +0330

    remove shadcn

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -1,40 +1,28 @@
 'use client'
-import { Button } from './ui/button'
+
 
 const HomeCards = () => {
   return (
     <section className="container">
-      <div>
-        <h1>test button: </h1>
-        <Button className="bg-primary" >Click me</Button>
-      </div>
       <div className='container-xl lg:container m-auto'>
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg'>
+
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg min-h-[400px] items-stretch'>
+
           <div className="p-6 rounded-lg shadow-md bg-accent">
             <h2 className='pacifico-regular text-5xl font-bold'>Instruments</h2>
             <p className='mt-2 mb-4'>
               Browse our React jobs and start your career today
             </p>
-            <a
-              to='/jobs'
-              className='inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700'
-            >
-              Let's play!
-            </a>
           </div>
+
           <div className="p-6 rounded-lg shadow-md bg-accent">
             <h2 className='text-5xl font-bold pacifico-regular'>music player</h2>
             <p className='mt-2 mb-4'>
               List your job to find the perfect developer for the role
             </p>
-            <a
-              to='/add-job'
-              className='inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600'
-            >
-             (find an appropriate name for this button)
-            </a>
           </div>
         </div>
+
       </div>
     </section>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,12 @@ import daisyui from "daisyui"
 
 
 export default {
-    darkMode: ['class'],
     content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+  	extend: {}
   },
   plugins: [ typography,
-    daisyui,
-      require("tailwindcss-animate")
+    daisyui
 ],
   daisyui: {
     themes: ["light", "dark", "dracula"]
