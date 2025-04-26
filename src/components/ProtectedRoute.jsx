4cf9caa4103b0594f7999c0a5ff63b230688e324
Mastle <amirali.tsh13@gmail.com>
2025-04-26 17:53:27 +0330

    Remove unnecessary comments

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,4 +1,3 @@
-import { useContext } from "react"
 import { Navigate, Outlet, useLocation } from "react-router"
 import { useAuth } from "./Auth/AuthContext"
 
@@ -6,17 +5,10 @@ const ProtectedRoute = () => {
   const { user } = useAuth()
   const location = useLocation()
 
-  // 1. While auth status is loading, optionally show a spinner
-  //   if (isLoading) {
-  //     return <div>Loading...</div>
-  //   }
-
-  // 2. If not authenticated, redirect to login, preserving where they were going
   if (!user) {
     return <Navigate to="/" replace state={{ from: location }} />
   }
 
-  // 3. Otherwise render child routes
   return <Outlet />
 }
 
